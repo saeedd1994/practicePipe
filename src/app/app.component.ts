96b59b8f6bd05df1faedb082af4204cfab777841
saeed <saeed.da1994@gmail.com>
2022-09-28 15:10:50 +0330

    test sample pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'practicePipes';
+  name: string = '';
+  date: string = '';
+  amount?:number;
+  mile?:number;
+
+  onInput($event: any) {
+    this.name=$event.target.value;
+  }
+
+  onChangeDate($event: any) {
+    this.date= $event.target.value;
+  }
+
+  onChangeAmount($event: any) {
+    this.amount=parseFloat($event.target.value)
+  }
+
+  onChangeMile($event: any) {
+    this.mile=parseFloat($event.target.value)
+  }
 }
---
+++ src/app/pipes/convert.pipe.ts
@@ -0,0 +1,16 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+  name: 'convert'
+})
+export class ConvertPipe implements PipeTransform {
+
+  transform(value: any, ...args: unknown[]): any {
+    if (!value)
+    {
+      return '';
+    }
+    return value * 1.60934;
+  }
+
+}
